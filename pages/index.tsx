740f80f8b49c19d54c8f1128014da064cc7b6d56
Olexiy <serboleksiy@gmail.com>
2022-09-11 18:48:52 +0300

    Swtich to typescript

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/layout/Layout";
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <h1>TSX start template</h1>
+    </Layout>
+  );
+};
+
+export default HomePage;
